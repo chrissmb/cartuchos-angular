153449850ff0a5ead7432210bfc8fd153202b09b
Christopher <chrissantista@gmail.com>
2017-11-29 22:25:15 -0200

    Alterado API para o Heroku.

--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -1,4 +1,5 @@
 'use strict';
 
 // tslint:disable-next-line:no-inferrable-types
-export const enderecoSite: string = 'http://127.0.0.1:8080/';
+// export const enderecoSite: string = 'http://127.0.0.1:8080/';
+export const enderecoSite: string = 'https://cartuchos.herokuapp.com/';

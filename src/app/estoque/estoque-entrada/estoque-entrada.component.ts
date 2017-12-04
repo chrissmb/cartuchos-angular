12f90654f71105f35f510efb479cec55bc3a6797
Christopher <chrissantista@gmail.com>
2017-12-03 22:33:34 -0200

    Criando estoque entrada.

--- src/app/estoque/estoque-entrada/estoque-entrada.component.ts
+++ src/app/estoque/estoque-entrada/estoque-entrada.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+
+import { Departamento } from '../../departamentos/departamento';
+import { DepartamentosService } from '../../departamentos/departamentos.service';
 
 @Component({
   selector: 'app-estoque-entrada',
   templateUrl: './estoque-entrada.component.html',
   styleUrls: ['./estoque-entrada.component.css']
 })
-export class EstoqueEntradaComponent implements OnInit {
+export class EstoqueEntradaComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  departamentos: Departamento[];
+  lista = [1,2,3,4,5];
+  subscription: Subscription;
+
+  constructor(private departamentosService: DepartamentosService) { }
+
+  getDepartamentos() {
+    this.departamentosService.getDepartamentosAtivos().subscribe(deptos => this.departamentos = deptos);
+  }
 
   ngOnInit() {
+    this.getDepartamentos();
+    this.subscription = this.departamentosService.recieveRefreshDepartamentos()
+        .subscribe(dados => this.getDepartamentos());
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/estoque/estoque.module.ts
+++ src/app/estoque/estoque.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, FormBuilder } from '@angular/forms';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { EstoqueRoutingModule } from './estoque-routing.module';
 import { EstoqueComponent } from './estoque.component';
 import { EstoqueEntradaComponent } from './estoque-entrada/estoque-entrada.component';
 import { EstoqueSaidaComponent } from './estoque-saida/estoque-saida.component';
+import { DepartamentosService } from '../departamentos/departamentos.service';
+import { DepartamentoResolver } from '../departamentos/departamento.resolver';
 
 @NgModule({
   imports: [
+    MaterializeModule,
+    FormsModule,
     CommonModule,
     EstoqueRoutingModule
   ],
@@ -15,6 +21,11 @@ import { EstoqueSaidaComponent } from './estoque-saida/estoque-saida.component';
     EstoqueComponent,
     EstoqueEntradaComponent,
     EstoqueSaidaComponent
+  ],
+  providers: [
+    DepartamentosService,
+    DepartamentoResolver,
+    FormBuilder
   ]
 })
 export class EstoqueModule { }

012ffd978d338fe3029378ae6f29f7ec4e06bd64
Christopher <chrissantista@gmail.com>
2017-12-07 21:29:11 -0200

    Incluso campo quantidade em entrada estouqe. Configurando validação de campos.

--- src/app/estoque/estoque-entrada/estoque-entrada.component.ts
+++ src/app/estoque/estoque-entrada/estoque-entrada.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Rx';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 import { Departamento } from '../../departamentos/departamento';
 import { DepartamentosService } from '../../departamentos/departamentos.service';
@@ -14,17 +15,33 @@ export class EstoqueEntradaComponent implements OnInit, OnDestroy {
   departamentos: Departamento[];
   lista = [1,2,3,4,5];
   subscription: Subscription;
+  formulario: FormGroup;
 
-  constructor(private departamentosService: DepartamentosService) { }
+  constructor(
+    private departamentosService: DepartamentosService,
+    private formBuilder: FormBuilder
+  ) { }
 
   getDepartamentos() {
     this.departamentosService.getDepartamentosAtivos().subscribe(deptos => this.departamentos = deptos);
+  }
+
+  onSubmit() {
+    if (this.formulario.valid) {
+      console.log('ok');
+    } else {
+      console.log('no no')
+    }
   }
 
   ngOnInit() {
     this.getDepartamentos();
     this.subscription = this.departamentosService.recieveRefreshDepartamentos()
         .subscribe(dados => this.getDepartamentos());
+    this.formulario = this.formBuilder.group({
+      cartucho: [null, Validators.required],
+      quantidade: [null, [Validators.required, Validators.min(1)]]
+    });
   }
 
   ngOnDestroy() {
--- src/app/estoque/estoque.module.ts
+++ src/app/estoque/estoque.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, FormBuilder } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterializeModule } from 'angular2-materialize';
 
 import { EstoqueRoutingModule } from './estoque-routing.module';
@@ -14,6 +14,7 @@ import { DepartamentoResolver } from '../departamentos/departamento.resolver';
   imports: [
     MaterializeModule,
     FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     EstoqueRoutingModule
   ],

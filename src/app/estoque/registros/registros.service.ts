a651bb8d1f7fc4a44ab0315d8289d7ba4780e8a9
Chris <chrissantista@gmail.com>
2017-12-30 20:20:21 -0200

    Escrevendo RegistrosService

--- src/app/estoque/registros/registros.service.ts
+++ src/app/estoque/registros/registros.service.ts
@@ -1,4 +1,8 @@
+import { Headers, Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import * as global from '../../shared/global';
 import { AuthService } from '../../login/auth.service';
@@ -7,6 +11,32 @@ import { Registro } from './registro';
 @Injectable()
 export class RegistrosService {
 
-  constructor() { }
+  private headers: Headers;
+  private subject = new Subject();
 
+  constructor(private http: Http, authService: AuthService) {
+    this.headers = authService.getHeaders();
+  }
+
+  getRegistros(): Observable<Registro[]> {
+    return this.http
+      .get(`${ global.enderecoSite }registros`, { headers: this.headers })
+      .map(response => response.json() as Registro[]);
+  }
+
+  getRegistro(id: number): Observable<Registro[]> {
+    return this.http
+      .get(`${ global.enderecoSite }registros/${id}`, { headers: this.headers })
+      .map(response => response.json() as Registro[]);
+  }
+
+  saveRegistro(registro: Registro): Observable<Registro> {
+    if (registro == null) {
+      return;
+    }
+    return this.http
+      .post(`${ global.enderecoSite }registros`,
+          JSON.stringify(registro), { headers: this.headers })
+      .map(response => response.json() as Registro);
+  }
 }
